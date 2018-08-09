2a63498458b54c419709bd95f63183eabffcf140
sharnajh <sharnajhossain@gmail.com>
2018-08-09 17:40:15 -0400

    Progress

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Owner.create(name: 'David', phone_number: '888-999-999', address: '907 john')
+Owner.create(name: 'Milly', phone_number: '777-999-999', address: '908 john')
+Owner.create(name: 'Kevin', phone_number: '666-999-999', address: '9087 john')
+
+
+Dog.create(name: 'Jazmine', age: 10, breed: 'Bothersome', owner_id: 1)
+Dog.create(name: 'Lisa', age: 9, breed: 'Bothersome', owner_id: 2)
+Dog.create(name: 'Tom', age: 20, breed: 'Awesome', owner_id: 3)
